e5f9cce14d0f96389b76363f64d245c1f9e03c82
Daffygzz <paulo_esilva@yahoo.com.br>
2022-05-18 09:43:07 -0300

    Enviroment

--- src/main/config/__init__.py
+++ src/main/config/__init__.py
@@ -1,1 +1,2 @@
 from .http_server_configs import app
+from .env_config import get_settings,Settings
--- src/main/routes/auth_routes.py
+++ src/main/routes/auth_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Request as RequestFastAPI
+from fastapi import Depends
 from src.validations.get_login_user_validator import get_login_user_validator
 from src.main.adapters.request_adapter import request_adapter
+from src.main.config.env_config import get_settings,Settings
 
 auth_routes: APIRouter = APIRouter()
 
@@ -17,3 +19,11 @@ def get_login_user(request: RequestFastAPI):
     else:
 
         return {"Message": "fail"}
+
+@auth_routes.get("/ping")
+async def ping(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
